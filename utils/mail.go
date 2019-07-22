57b28d11cc5a8a0eb3cbf6d34adb508e9850fc87
sankamille <luc.brulet@epitech.eu>
2019-07-22 17:27:10 +1000

    [+] Create a function that handle the sending mail action

--- utils/mail.go
+++ utils/mail.go
@@ -2,42 +2,34 @@ package utils
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"net/smtp"
-	"os"
 
 	"github.com/jordan-wright/email"
 	"github.com/lbrulet/APINIT-GO/configs"
 	"github.com/lbrulet/APINIT-GO/models"
 )
 
-type Template struct {
-	Email        string
-	Username     string
-	ConfirmEmail string
-}
-
-func SendMail(user models.User, token string) {
-	if pwd, err := os.Getwd(); err != nil {
-		panic(err)
+// SendMail is used to send an email
+func SendMail(user models.User, formMail interface{}, pathTotemplate string) {
+	if tmpl, err := template.ParseFiles(pathTotemplate); err != nil {
+		fmt.Printf("[WARNING] %s!\n", err)
 	} else {
-		if tmpl, err := template.ParseFiles(pwd + "/templates/welcome.html"); err != nil {
-		} else {
-			data := Template{
-				Email:        user.Email,
-				Username:     user.Username,
-				ConfirmEmail: configs.Config.MailConfirmationLink + "?token=" + token,
+		var tpl bytes.Buffer
+		tmpl.Execute(&tpl, formMail)
+		e := email.NewEmail()
+		e.From = configs.Config.MailFrom
+		e.To = []string{user.Email}
+		e.Subject = configs.Config.MailSubject
+		e.HTML = []byte(tpl.String())
+		go func() {
+			if len(configs.Config.MailAddress) != 0 && len(configs.Config.MailPassword) != 0 {
+				e.Send(configs.Config.SMTPAddress+":"+configs.Config.SMTPPort, smtp.PlainAuth("", configs.Config.MailAddress,
+					configs.Config.MailPassword, configs.Config.SMTPAddress))
+			} else {
+				fmt.Printf("[WARNING] You didn't load your credentials into the environnement!\n")
 			}
-			var tpl bytes.Buffer
-			tmpl.Execute(&tpl, data)
-			e := email.NewEmail()
-			e.From = configs.Config.MailFrom
-			e.To = []string{user.Email}
-			e.Subject = configs.Config.MailSubject
-			e.HTML = []byte(tpl.String())
-			go func() {
-				e.Send(configs.Config.SMTPAddress, smtp.PlainAuth("", "ADDRESSGOOGLE", "PASSWORD", "smtp.gmail.com"))
-			}()
-		}
+		}()
 	}
 }
